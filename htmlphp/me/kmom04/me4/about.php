bbcb3cacd822dca584a35568977985b120ae1740
Kenneth Lewenhagen <lewenhagen@live.com>
2017-09-13 09:36:28 +0200

    added kmom04

---
+++ htmlphp/me/kmom04/me4/about.php
@@ -0,0 +1,7 @@
+<?php
+$title = "Om";
+include("incl/header.php"); ?>
+    <h1>Om sidan</h1>
+    <p>Lite information om sidan.</p>
+
+<?php include("incl/footer.php"); ?>
---
+++ htmlphp/me/kmom04/me4/get-post.php
@@ -0,0 +1,5 @@
+<?php
+$title = "get post";
+include("incl/header.php");
+
+include("incl/footer.php");

1c9ff06e1a5765317ff3003423a4d15e93f1c4da
Kenneth Lewenhagen <lewenhagen@live.com>
2018-11-30 08:12:51 +0100

    asdf

--- javascript1/kmom04/flag/js/main.js
+++ javascript1/kmom04/flag/js/main.js
@@ -1,189 +1,67 @@
 (function () {
     "use strict";
 
-    var content = document.getElementById("content");
+    var arr1 = ["Kenneth", "Andreas", "Emil"];
+    // arr1.unshift("Mikael");
 
-    var animal = {
-        "species": "",
-        "sound": "",
-        "color": "",
-        "init": function(species, sound, color) {
-            this.species = species;
-            this.sound = sound;
-            this.color = color;
-        },
-        print: function() {
-            return "The " + this.color + " " + this.species + " says " + this.sound;
-        },
-        1: "lala",
-    };
-    // function superclass(name) {
-    //     this.name = name;
-    //     this.print = function() {
-    //         console.log(this.name);
+    // arr1.forEach(function(value) {
+    //     console.log(value);
+    // });
+
+    //
+    // var myObj = {
+    //     x: 42,
+    //     getX: function() {
+    //         console.log(this.x);
     //     }
     // }
-    // var test = new superclass("kalle");
-    // test.print();
-    // var Cow = Object.create(animal);
     //
-    // Cow.species = "cow";
-    // Cow.sound = "moo";
-    // Cow.color = "brown";
+    // var test = myObj.getX.bind(myObj);
     //
-    // var Mouse = Object.create(animal);
-    //
-    // Mouse.species = "mouse";
-    // Mouse.sound = "piip";
-    // Mouse.color = "gray";
-    //
-    // content.innerHTML = Cow.print();
-    // content.innerHTML += "<br>" + Mouse.print();
+    // test();
 
-    // var Cow = Object.create(animal);
-    // Cow.init("cow", "moo", "brown");
-    // delete Cow.sound;
-    //
-    //
-    // var Mouse = Object.create(animal);
-    // Mouse.init("mouse", "piip", "gray");
-    //
-    // content.innerHTML = Cow.print();
-    // content.innerHTML += "<br>" + Mouse.print();
-
-    // var person = {};
-    //
-    // person["name"] = "Kalle";
-    // person.age = 25;
-    //
-    // console.log(person.name);
-    // console.log(person["age"]);
-    //
-    // person.present = function(){
-    //     return this.name + " is " + this.age + " years old.";
-    // };
-    //
-    // console.log(person["present"]());
-    // console.log(person.present());
-
-    // var person = {
-    //     "name": "",
-    //     "age": 0,
-    //     "init": function(name, age) {
-    //         this.name = name;
-    //         this.age = age;
-    //     },
-    //     "present": function() {
-    //         return this.name + " is " + this.age + " years old.";
-    //     }
-    // };
-    //
-    // var car = {
-    //     "model": "",
-    //     "color": "",
-    //     "owner": {},
-    //     "init": function(model, color, owner) {
-    //         this.model = model;
-    //         this.color = color;
-    //         this.owner = owner;
-    //     },
-    //     "present": function() {
-    //         return "The " + this.color + " " + this.model + " is owned by: " + this.owner.name;
-    //     }
-    // };
-    //
-    // var kalle = Object.create(person);
-    // kalle.init("Kalle", 25);
-    //
-    // console.log(kalle.present());
-    //
-    // var lisa = Object.create(person);
-    // lisa.init("Lisa", 28);
-    // console.log(lisa.present());
-    //
-    // var volvo = Object.create(car);
-    // volvo.init("Volvo", "blue", kalle);
-    //
-    // console.log(volvo.present());
-    function Bag() {
-        var items = [];
-        this.addItem = function(item) {
-            items.push(item);
-        };
-        this.look = function() {
-            return items.toString();
-        }
-    }
-
-    function Person(name, age, bag) {
-        this.bag = bag;
+    function Person(name, friends) {
         this.name = name;
-        this.age = age;
-        this.present = function() {
-            return this.name + " is " + this.age + " years old.";
-        };
-        this.look = function() {
-            return this.bag.look();
-        }
+        this.friends = friends;
     }
-    var bag1 = new Bag();
-    var bag2 = new Bag();
 
-    console.log(bag.items);
+    Person.prototype.friendsList = function() {
 
-    var kalle = new person("Kalle", 25, bag1);
-    var lisa = new person("Lisa", 28, bag2);
+        var message = this.friends.map(function(value) {
+            return this.name + " is friends with " + value;
+        }.bind(this));
+        return message;
+    }
 
-    kalle.bag.addItem("Kaffekopp");
-    kalle.bag.addItem("Tallrik");
-    kalle.bag.addItem("Gaffel");
+    var me = new Person("Kenneth", ["Emil", "Andreas", "Mikael"]);
 
-    lisa.bag.addItem("gris");
-    lisa.bag.addItem("katt");
-    lisa.bag.addItem("häst");
+    console.log(me.friendsList());
 
-    console.log(kalle.look());
-    console.log(lisa.look());
-    console.log();
-
-
-    // console.log(lisa.present());
-
-
-    // var flag = {
-    //     html: "",
-    //     name: "",
-    //     init: function (flag) {
-    //         this.name = flag;
-    //         this.html = "<div class='" + flag + "'><div class='part1'></div><div class='part2'></div></div>";
-    //     },
-    //     draw: function () {
-    //         return this.html;
-    //     }
-    // };
+    // console.log(arr1[1]);
+    // console.log(arr1.length);
+    // console.log(arr1[arr1.length - 1]);
     //
-    // var sweden = Object.create(flag);
-    // sweden.init("sweden");
-    // var benin = Object.create(flag);
-    // benin.init("benin");
-    // var colombia = Object.create(flag);
-    // colombia.init("colombia");
-    // var finland = Object.create(flag);
-    // finland.init("finland");
     //
-    // var allFlags = [sweden, benin, colombia, finland];
     //
-    // for (var i = 0; i < allFlags.length; i++) {
-    //     var flagElement = document.createElement("div");
-    //     flagElement.className = "flag";
-    //     flagElement.innerHTML = allFlags[i].draw();
+    // var arr2 = arr1.slice();
     //
-    //     flagElement.addEventListener("click", function() {
-    //         this.style.display = "none";
-    //     });
+    // arr1[0] = "Mikael";
     //
-    //     content.appendChild(flagElement);
-    // }
+    // console.log(arr1);
+    // console.log(arr2);
 
-    // console.log("Sandbox ready.");
+    // var arr3 = new Array(3);
+    // arr3[100] = "test";
+    // console.log(arr3);
+
+    // var arr1 = [1, 5, 6, 7];
+    // var result = arr1.reduce(function(total, val) {
+    //     total += val;
+    //     return total;
+    // });
+    //
+    // console.log(result);
+
+
+
 })();
